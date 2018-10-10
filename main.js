025adab5ed4a69f43987d5387bb3760801ed14cf
sbasu <shantanubasu123@gmail.com>
2018-10-10 17:16:21 +0530

    third commit

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-Vue.component(
-  'modal',{
-  template:`
-  <div class="modal is-active">
-    <div class="modal-background"></div>
-    <div class="modal-content box">
-      <slot></slot>
-    </div>
-    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
-  </div>
-  `
-});
-
 new Vue({
-  el: '#root',
-  data:{
-    display:false,
-  },
-  methods:{
-    showModal(){
-        return this.display = true;
-    }
-
-  }
+  el: "#root",
 });
